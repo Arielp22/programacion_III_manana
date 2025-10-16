bbb8e5e149ce1d55bfbfe3c80f61c6a9a0695489
Arielp22 <earielp2000@hotmail.com>
2025-10-16 09:56:27 -0500

    Clase 16-10-2025

---
+++ Modulo2/javascript/02_variables.js
@@ -0,0 +1,34 @@
+console.log("Variables");
+
+saludo="Hola utilizando variable"
+console.log("Variable saludo",saludo);
+console.log("Tipo de variable", typeof(saludo));
+
+numero=1234
+console.log("Variable numero",numero);
+console.log("Tipo de variable", typeof(numero));
+
+booleano=true
+console.log("Variable booleano",booleano);
+console.log("Tipo de variable", typeof(booleano));
+
+indefinida=undefined
+console.log("Variable indefinida",indefinida);
+console.log("Tipo de variable", typeof(indefinida));
+
+nula=null
+console.log("Variable nula",nula);
+console.log("Tipo de variable", typeof(nula));
+
+bigentero=12345678901234567n
+console.log("Variable bigentero",bigentero);
+console.log("Tipo de variable", typeof(bigentero));
+
+persona={nombre:"Juan", edad:30, activo:true}
+console.log("Variable persona",persona);
+console.log("Tipo de variable", typeof(persona));
+
+numeros=[10,20,30,40,50]
+console.log("Variable numeros",numeros);
+console.log("Tipo de variable", typeof(numeros));
+
